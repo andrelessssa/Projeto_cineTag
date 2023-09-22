0589ec21848ece2a51acd0c06454c821e024c68d
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2023-09-22 19:51:15 -0300

    banner
    add Banner

--- cinetag/src/pages/Inicio/index.js
+++ cinetag/src/pages/Inicio/index.js
@@ -1,9 +1,16 @@
+import Banner from "components/banner";
 import Cabecalho from "components/cabecalho";
+import Rodape from "components/rodape";
 
 
 function Inicio(){
     return(
+    <>
        <Cabecalho />
+       <Banner imagem="home"/>
+       <Rodape />
+       
+    </>
     )
 };
 export default Inicio;
